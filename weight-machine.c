26f660ad9aa4633c4aa30b066f9ef14f4b53678f
Martina Schwerdtfeger <codekrams@gmail.com>
2018-05-22 20:57:56 +0200

    correction of c-file

--- weight-machine.c
+++ weight-machine.c
@@ -10,11 +10,15 @@ int main()
     printf("Bitte geben Sie das Gewicht der Ware ein von 0 bis 127 kg:\n");
     scanf("%i", &gewichtseingabe);
 
+    if (gewichtseingabe>0 && gewichtseingabe<128){
     while(gewichtseingabe>0){
-        gewichtszaehler=gewichtseingabe/gewichtsstein;
-        printf("%i %i-Kilo-Gewicht\n", gewichtszaehler, gewichtsstein);
-        gewichtseingabe=gewichtseingabe%gewichtsstein;
-        gewichtsstein=gewichtsstein/2;
-
+            gewichtszaehler=gewichtseingabe/gewichtsstein;
+            printf("%i %i-Kilo-Gewicht\n", gewichtszaehler, gewichtsstein);
+            gewichtseingabe=gewichtseingabe%gewichtsstein;
+            gewichtsstein=gewichtsstein/2;
+        }
+        }
+    else {
+        printf("Fuer dieses Gewicht sind keine Gewichte vorhanden");
     }
 }
